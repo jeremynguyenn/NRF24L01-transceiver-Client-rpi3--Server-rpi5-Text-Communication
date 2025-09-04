d8217c3fae4715a0a05356aa88f109217fb5e0ab
Nguyễn Trung Nhân <112737822+jeremynguyenn@users.noreply.github.com>
2025-09-04 21:20:38 +0700

    Update tx_stdin.c

--- NRF24L01_RF/EXAMPLES/tx_stdin.c
+++ NRF24L01_RF/EXAMPLES/tx_stdin.c
@@ -1,6 +1,6 @@
 /**
- * Author: Christopher Stewart (Christopher.ray.stewart@gmail.com)
- * Date: 05062024
+ * Author: Nguyen Nhan
+ * Date: 30042025
  * Description: NRF24L01 program to transmit on a NRF24 transceiver via stdin,
  * 
  * gcc -o tx_stdin tx_stdin.c -lgpiod
